fd7e38888ea4c9999c9f4a0b5685d83074a24267
Rafael <rafael.martins.hipolito@gmail.com>
2026-02-27 18:32:06 +0000

    navbar

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -1,10 +1,16 @@
 export const translations = {
   en: {
     title: "Welcome",
-    button: "Switch to Portuguese"
+    button: "Switch to Portuguese",
+    sponsors: "Sponsors",
+    aboutUs: "About us",
+    contacts: "Contacts"
   },
   pt: {
     title: "Bem vindo",
-    button: "Trocar para inglês"
+    button: "Trocar para inglês",
+    sponsors: "Patrocinadores",
+    aboutUs: "Sobre nós",
+    contacts: "Contactos"
   }
 };
