16f754ff6ab3c39843ed4be3a27dc6e66b0d4469
Rafael <rafael.martins.hipolito@gmail.com>
2026-03-01 19:51:21 +0000

    footer

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -4,13 +4,17 @@ export const translations = {
     button: "Switch to Portuguese",
     sponsors: "Sponsors",
     aboutUs: "About us",
-    contacts: "Contacts"
+    contacts: "Contacts",
+    home: "Home Page",
+    location: "Location"
   },
   pt: {
     title: "Bem vindo",
     button: "Trocar para inglês",
     sponsors: "Patrocinadores",
     aboutUs: "Sobre nós",
-    contacts: "Contactos"
+    contacts: "Contactos",
+    home: "Página principal",
+    location: "Localização"
   }
 };
