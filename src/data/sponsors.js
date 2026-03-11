917f192d920a93aabf59914f82c169ee5baf8c90
Rafael <rafael.martins.hipolito@gmail.com>
2026-03-11 19:32:57 +0000

    setting up for sponsor page

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,4 +1,305 @@
 export const sponsors =[
-
+    {
+  platinum: [
+    {
+      "altName": "ISEL",
+      "link": "https://www.isel.pt",
+      "image": "../sponsor_logos/isel.svg"
+    },
+    {
+      "altName": "IPL",
+      "link": "https://www.ipl.pt",
+      "image": "../sponsor_logos/ipl.svg"
+    },
+    {
+      "altName": "Grupo Metal",
+      "link": "https://grupometal.com",
+      "image": "../sponsor_logos/grupo_metal.svg"
+    },
+    {
+      "altName": "Infusão",
+      "link": "https://www.infusao.pt",
+      "image": "../sponsor_logos/infusao.webp"
+    },
+    {
+      "altName": "Facom",
+      "link": "https://www.facom.com/",
+      "image": "../sponsor_logos/facom.webp"
+    },
+    {
+      "altName": "Cubotónic",
+      "link": "https://www.cubotonic.pt",
+      "image": "../sponsor_logos/cubotonic.webp"
+    },
+    {
+      "altName": "Almeida & Lamas",
+      "link": "https://almeida-lamas.pt/",
+      "image": "../sponsor_logos/Almeida_Lamas.svg"
+    }
+  ],
+  gold: [
+    {
+      "altName": "Coficab",
+      "link": "https://www.coficab.pt/",
+      "image": "../sponsor_logos/Coficab.webp"
+    },
+    {
+      "altName": "Leomavel Moldes",
+      "link": "http://leomavel.pt",
+      "image": "../sponsor_logos/leomavel.webp"
+    },
+    {
+      "altName": "MJBranco",
+      "link": "http://www.mjbranco.pt",
+      "image": "../sponsor_logos/mjbranco.webp"
+    },
+    {
+      "altName": "Reiman",
+      "link": "https://reiman.pt",
+      "image": "../sponsor_logos/reiman.svg"
+    },
+    {
+      "altName": "Tesla",
+      "link": "https://www.tesla.com/",
+      "image": "../sponsor_logos/tesla.svg"
+    },
+    {
+      "altName": "Berner",
+      "link": "https://shop.berner.eu/pt-pt",
+      "image": "../sponsor_logos/berner.svg"
+    },
+    {
+      "altName": "3D-ISO",
+      "link": "https://www.3d-iso.pt",
+      "image": "../sponsor_logos/3d_iso.svg"
+    },
+    {
+      "altName": "CicloFapril",
+      "link": "https://ciclofapril.pt/pt",
+      "image": "../sponsor_logos/CICLOFAPRIL.svg"
+    },
+    {
+      "altName": "xiraplas",
+      "link": "https://www.xiraplas.pt",
+      "image": "../sponsor_logos/xiraplas.svg"
+    }
+  ],
+  silver: [
+    {
+      "altName": "Eurocircuits",
+      "link": "https://www.eurocircuits.com",
+      "image": "../sponsor_logos/euro_circuits.webp"
+    },
+    {
+      "altName": "Deconorma",
+      "link": "https://deconorma.com",
+      "image": "../sponsor_logos/deconorma.webp"
+    },
+    {
+      "altName": "Jdeus",
+      "link": "https://www.jdeus.com/",
+      "image": "../sponsor_logos/jdeus.svg"
+    },
+    {
+      "altName": "RMCengrenagens",
+      "link": "https://rmc.com.pt/en/company",
+      "image": "../sponsor_logos/RMC_ORIGINAL.svg"
+    },
+    {
+      "altName": "Ribermold",
+      "link": "https://www.ribermold.pt",
+      "image": "../sponsor_logos/Ribermold.svg"
+    },
+    {
+      "altName": "Casauto",
+      "link": "https://casauto.lojasonlinectt.pt/",
+      "image": "../sponsor_logos/Casauto.webp"
+    },
+    {
+      "altName": "Critical Techworks",
+      "link": "https://www.criticaltechworks.com/",
+      "image": "../sponsor_logos/ctw.svg"
+    },
+    {
+      "altName": "TE Connectivity",
+      "link": "https://www.te.com",
+      "image": "../sponsor_logos/te_connectivity.svg"
+    }
+  ],
+  bronze: [
+    {
+      "altName": "VL Moldes",
+      "link": "https://www.vlmoldes.pt",
+      "image": "../sponsor_logos/vl_moldes.svg"
+    },
+    {
+      "altName": "Sabino",
+      "link": "https://sabino.pt",
+      "image": "../sponsor_logos/Sabino.webp"
+    },
+    {
+      "altName": "RebelCo",
+      "link": "https://rebelco.pt",
+      "image": "../sponsor_logos/Rebelco.webp"
+    },
+    {
+      "altName": "Carlis",
+      "link": "http://www.carlis.pt",
+      "image": "../sponsor_logos/carlis.webp"
+    },
+    {
+      "altName": "Cadflow",
+      "link": "https://www.cadflow.pt/index.php",
+      "image": "../sponsor_logos/Cadflow.svg"
+    },
+    {
+      "altName": "Vigrade",
+      "link": "https://www2.news.vi-grade.com/e/832623/2025-01-13/2cdft7/894804871/h/GJKopkmegudU6l7fOWnryJc5KiKF2YJa8B3CDOn6X0s",
+      "image": "../sponsor_logos/vi-grade_red.svg"
+    },
+    {
+      "altName": "Ramada Aços",
+      "link": "https://www.ramada.pt",
+      "image": "../sponsor_logos/ramada_acos.svg"
+    },
+    {
+      "altName": "Loctite",
+      "link": "https://www.henkel-adhesives.com/pt/pt/empresa/as-nossas-marcas/loctite-new-we.html",
+      "image": "../sponsor_logos/loctite.webp"
+    },
+    {
+      "altName": "Mauser.pt",
+      "link": "https://mauser.pt/",
+      "image": "../sponsor_logos/mauser.webp"
+    },
+    {
+      "altName": "ThinkOpen",
+      "link": "https://thinkopen.solutions",
+      "image": "../sponsor_logos/ThinkOpen.webp"
+    },
+    {
+      "altName": "KVASER",
+      "link": "https://kvaser.com",
+      "image": "../sponsor_logos/Kvaser.webp"
+    }
+  ],
+  partners: [
+    {
+      "altName": "Simscale",
+      "link": "https://www.simscale.com/",
+      "image": "../sponsor_logos/SimScale.webp"
+    },
+    {
+      "altName": "Rus",
+      "link": "https://www.rohde-schwarz.com/pt/home_48230.html",
+      "image": "../sponsor_logos/rus-logo.svg"
+    },
+    {
+      "altName": "Ansys",
+      "link": "https://www.ansys.com",
+      "image": "../sponsor_logos/ansys.svg"
+    },
+    {
+      "altName": "Altium",
+      "link": "https://www.altium.com",
+      "image": "../sponsor_logos/altium.svg"
+    },
+    {
+      "altName": "Copisel",
+      "link": "https://www.copisel.pt",
+      "image": "../sponsor_logos/copisel.svg"
+    },
+    {
+      "altName": "EasyComposites",
+      "link": "https://www.easycomposites.eu/",
+      "image": "../sponsor_logos/easy_composites.webp"
+    },
+    {
+      "altName": "Carpintaria Lino e Filhos",
+      "link": "http://linoefilhos.pt",
+      "image": "../sponsor_logos/carpintaria_lino_e_filhos.webp"
+    },
+    {
+      "altName": "Atomic Shop",
+      "link": "https://www.atomic-shop.pt/",
+      "image": "../sponsor_logos/atomic_shop.svg"
+    },
+    {
+      "altName": "AEISEL",
+      "link": "https://www.facebook.com/AEISEL.1984",
+      "image": "../sponsor_logos/aeisel.webp"
+    },
+    {
+      "altName": "CRM",
+      "link": "https://www.crm-motorsport.com/",
+      "image": "../sponsor_logos/CRM.webp"
+    },
+    {
+      "altName": "CMFonseca",
+      "link": "https://www.cmfonseca.com",
+      "image": "../sponsor_logos/Fonseca.svg"
+    },
+    {
+      "altName": "Rapid Harness",
+      "link": "https://rapidharness.com/",
+      "image": "../sponsor_logos/rapidharness.webp"
+    },
+    {
+      "altName": "Solidworks",
+      "link": "https://www.solidworks.com",
+      "image": "../sponsor_logos/solidworks.svg"
+    },
+    {
+      "altName": "Enepaq",
+      "link": "https://enepaq.com/",
+      "image": "../sponsor_logos/enepaq.webp"
+    },
+    {
+      "altName": "Schroth Racing",
+      "link": "https://www.schrothracing.com/",
+      "image": "../sponsor_logos/Schroth.webp"
+    },
+    {
+      "altName": "Frade Metarlúgica",
+      "link": "https://frademetalurgica.pt/",
+      "image": "../sponsor_logos/frade.webp"
+    },
+    {
+      "altName": "Calspan",
+      "link": "https://calspan.com",
+      "image": "../sponsor_logos/Calspan.webp"
+    },
+    {
+      "altName": "Bender",
+      "link": "https://www.bender.de",
+      "image": "../sponsor_logos/bender.svg"
+    },
+    {
+      "altName": "Eurocorreias",
+      "link": "https://eurocorreias.pt",
+      "image": "../sponsor_logos/eurocorreias.webp"
+    },
+    {
+      "altName": "MViera",
+      "link": "https://mvieira.pt/",
+      "image": "../sponsor_logos/mvieira.webp"
+    },
+    {
+      "altName": "MathWorks",
+      "link": "https://www.mathworks.com/",
+      "image": "../sponsor_logos/mathworks.svg"
+    },
+    {
+      "altName": "Auto Sock",
+      "link": "https://autosock.com/",
+      "image": "../sponsor_logos/auto_sock.webp"
+    },
+    {
+      "altName": "Camnetics",
+      "link": "https://camnetics.com",
+      "image": "../sponsor_logos/camnetics.webp"
+    }
+  ]
+}
     
 ]
