f4d6d97d359857ef6833c231521a7c32f81e7d67
Rafael <rafael.martins.hipolito@gmail.com>
2026-03-14 19:10:28 +0000

    fixes

---
+++ src/data/about_us.js
@@ -0,0 +1,8 @@
+export const text = {
+    pt:{
+        
+    },
+    en:{
+
+    }
+}
--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -2,35 +2,55 @@ export const translations = {
   en: {
     title: "Welcome",
     button: "Switch to Portuguese",
+    home: "Home Page",
+    /* sponsors */
     sponsors: "SPONSORS",
     platinum: "PLATINUM",
     gold: "GOLD",
     silver: "SILVER",
     bronze: "BRONZE",
     partners: "PARTNERS",
+    /* about us */
     aboutUs: "ABOUT US",
-    contacts: "CONTACTS",
-    home: "Home Page",
     location: "Location",
-    submit: "SUBMIT",
     interested: "Interested in help ?",
-    contact_us: "CONTACT US"
+    contact_us: "CONTACT US",
+    competition:"The competition",
+    project:"The project",
+    /* contacts */
+    contacts: "CONTACTS",
+    emailTxt: "Enter your email",
+    entityName:"Entity name",
+    entityTxt:"Enter your entity name",
+    writeMsg:"Write you message",
+    writeTxt:"Write something here",
+    submit: "SUBMIT",
   },
   pt: {
     title: "Bem vindo",
     button: "Trocar para inglês",
+    home: "Página principal",
+    /* sponsors */
     sponsors: "PATROCINADORES",    
     platinum: "PLATINA",
     gold: "OURO",
     silver: "PRATA",
     bronze: "BRONZE",
     partners: "PARCEIROS",
+    /* about us */
     aboutUs: "SOBRE NÓS",
-    contacts: "CONTACTOS",
-    home: "Página principal",
     location: "Localização",
-    submit: "ENVIAR",
     interested: "Interessado em ajudar ?",
-    contact_us: "CONTACTE-NOS"
+    contact_us: "CONTACTE-NOS",
+    competition:"A competição",
+    project:"O projeto",
+    /* contacts */
+    contacts: "CONTACTOS",
+    emailTxt: "Insira o seu email",
+    entityName:"Nome da entidade",
+    entityTxt:"Insira o nome da entidade",
+    writeMsg:"A sua mensagem",
+    writeTxt:"Escreve algo...",
+    submit: "ENVIAR",
   }
 };
