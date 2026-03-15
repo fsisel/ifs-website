744465c1880454580427a40c0777eda1c12bc99e
Rafael <rafael.martins.hipolito@gmail.com>
2026-03-15 18:50:38 +0000

    ifs05 finnished

---
+++ src/data/car.js
@@ -0,0 +1,27 @@
+export const text ={
+    pt:{
+        text1:"O IFS05 é o último veículo elétrico desenvolvido pela equipa ISEL Formula Student.",
+        text2:"Projetado a pensar no peso reduzido e fiabilidade, o IFS05 atinge o equilíbrio perfeito entre desempenho e agilidade.",
+        electric:"Elétrico",
+        performance:"Alto Desempenho",
+        agility:"Máxima Agilidade",
+        chassis:"Chassis Tubular",
+        body:"Body em Fibra de Carbono",
+        suspension:"Suspensão Pneumática",
+        power:"POTÊNCIA",
+        weight:"PESO"
+    },
+    en:{
+        text1:"IFS05 is the latest fully electric vehicle developed by ISEL Formula Student.",
+        text2:"Designed with lightness and reliability in mind IFS05 strikes the perfect balance between performance and agility.",
+        electric:"Electric",
+        performance:"High Performance",
+        agility:"Maximum Agility",
+        chassis:"Tubular Space Frame Chassis",
+        body:"Carbon Fiber Body",
+        suspension:"Pneumatic Suspention",
+        power:"POWER",
+        weight:"WEIGHT"
+    }
+
+}
