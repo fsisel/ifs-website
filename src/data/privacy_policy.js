19da54c393b91c9d762a56205715d671a3c0b7e5
Rafael <rafael.martins.hipolito@gmail.com>
2026-04-05 12:33:22 +0100

    privacy policy page

---
+++ src/data/privacy_policy.js
@@ -0,0 +1,15 @@
+export const privacyPolicyTexts = {
+    pt:{
+        l1:"Na ISEL Formula Student uma das nossas prioridades é a privacidade dos nossos utilizadores. Esta política de privacidade contém uma descrição da informação recolhida pelo website da ISEL Formula Student e como nós a utilizamos.",
+        l2:"A ISEL Formula Student segue um protocolo de utilização de ficheiros estatísticos. Estes ficheiros são gerados quando o utilizador visita o website e fazem parte do serviço de alojamento do website. A informação dos utilizadores recolhida por estes ficheiros inclui endereços IP, o navegador web, o fornecedor de internet do utilizador (ISP) e data e hora dos acessos. Nenhum destes dados é relacionado com dados dos utilizadores pessoalmente identificáveis. O propósito da recolha desta informação é para análise estatística e gestão do website da ISEL Formula Student.",
+        questions:"Se tiver alguma questão relacionada com a nossa política de privacidade contacte-nos através do endereço <a className='link' href='mailto:formulastudent@isel.pt'>formulastudent@isel.pt</a>",
+        concent:"Ao utilizar o nosso website declara que tomou conhecimento e aceita a nossa política de privacidade.",
+    },
+    en:{
+        l1:"At ISEL Formula Student one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that are collected and recorded by ISEL Formula Student and how we use it.",
+        l2:"ISEL Formula Student follows a standard procedure of using log files. These files log visitors when they visit websites and are a part of hosting services analytics. The information collected by log files include Internet Protocol (IP) addresses, browser type, internet service provider (ISP) and time stamp. These are not linked to any information that is personally identifiable. The purpose of the information is for analysing trends, administering the site and gathering demographic information.",
+        questions:"If you have additional questions or require more information about our Privacy Policy, contact us at <a class='link' href='mailto:formulastudent@isel.pt'>formulastudent@isel.pt</a>",
+        concent:"By using our website, you hereby consent to our Privacy Policy.",
+    }
+
+}
