114237b3e6838e6cd2c3fce2b8972c10a67219cf
raf4570 <rafael.martins.hipolito@gmail.com>
2026-07-05 15:04:31 +0100

    garage updates

---
+++ src/data/garage.js
@@ -0,0 +1,284 @@
+export const garage ={
+    pt:{
+        ifs06:{
+            nickname:"IFS06",
+            description:"O mais veloz",
+            media: {
+                thumbnail: "/images/ifs05-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            
+            performance:{
+                Top_Speed:"TBA",
+                O_to_hundred:"4.2s",
+                Weight:"TBD",
+            },
+            powertrain:{
+                Motor:"(algo)",
+                Power:"80kW (107cv)",
+                Battery:"LiCoO2 (Personalizada)",
+            },
+            suspension:"Sistema de suspensão de duplo triângulo otimizado para condições de pista.",
+            
+            chassis:"Chassis tubular tipo spaceframe concebido para máxima rigidez e segurança em curvas de alta velocidade.",
+        },
+        ifs05:{
+            nickname:"IFS05",
+            description:"O mais veloz",
+            media: {
+                thumbnail: "/images/ifs05-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"TBA",
+                O_to_hundred:"4.2s",
+                Weight:"230kg",
+            },
+            powertrain:{
+                Motor:"(algo)",
+                Power:"80kW (107cv)",
+                Battery:"LiCoO2 (Personalizada)",
+            },
+            suspension:"Sistema de suspensão de duplo triângulo otimizado para condições de pista.",
+            
+            chassis:"Chassis tubular tipo spaceframe concebido para máxima rigidez e segurança em curvas de alta velocidade.",
+        },
+        ifs04:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs04-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs03:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs03_right.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs02:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs02_2.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs01:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs01_2.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+    },
+    en:{
+        ifs06:{
+            nickname:"IFS06",
+            description:"The fastest",
+            media: {
+                thumbnail: "/images/ifs05-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"TBA",
+                O_to_hundred:"4.2s",
+                Weight:"TBD",
+            },
+            powertrain:{
+                Motor:"(something)",
+                Power:"80kW (107hp)",
+                Battery:"LiCoO2 (Custom)",
+            },
+            suspension:"Double wishbone suspension system optimized for track conditions.",
+            
+            chassis:"Tubular spaceframe designed for maximum rigidity and safety during high-speed cornering.",
+        },
+        ifs05:{
+            nickname:"IFS05",
+            description:"The fastest",
+            media: {
+                thumbnail: "/images/ifs05-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"TBA",
+                O_to_hundred:"4.2s",
+                Weight:"230kg",
+            },
+            powertrain:{
+                Motor:"(something)",
+                Power:"80kW (107hp)",
+                Battery:"LiCoO2 (Custom)",
+            },
+            suspension:"Double wishbone suspension system optimized for track conditions.",
+            
+            chassis:"Tubular spaceframe designed for maximum rigidity and safety during high-speed cornering.",
+        },
+        ifs04:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs04-lateral.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs03:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs03_right.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs02:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs02_2.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+        ifs01:{
+            nickname:"",
+            description:"",
+            media: {
+                thumbnail: "/images/ifs01_2.webp",
+                model: "/models/porsche/scene.gltf"
+            },
+            performance:{
+                Top_Speed:"",
+                O_to_hundred:"",
+                Weight:"",
+            },
+            powertrain:{
+                Motor:"",
+                Power:"",
+                Battery:"",
+            },
+            suspension:{
+                text:""
+            },
+            chassis:{
+                text:""
+            },
+        },
+    }
+
+}
