07d8c4c777102a4309f37b92d0f30f23c0b08103
raf4570 <rafael.martins.hipolito@gmail.com>
2026-07-29 15:50:21 +0100

    rollout page updates

---
+++ src/data/rollout.js
@@ -0,0 +1,10 @@
+export const text ={
+    pt:{
+        text:"Um enorme obrigado a todos os que se juntaram a nós no nosso evento de lançamento! Ficámos muito felizes por ver tantas caras conhecidas e por partilhar a nossa visão convosco. Ter o vosso apoio neste passo tão importante significa muito para nós. Obrigado por terem tornado este dia verdadeiramente especial!"
+    },
+    en:{
+        text:"A massive thank you to everyone who joined us for our rollout event! We were thrilled to see so many familiar faces and to share our vision with you. Having your support as we take this next big step means the world to us. Thank you for making the day truly special!"
+        
+    }
+
+}
